667f371dad419387e0c5222fb0704b8ca24db381
Mitul Kanani <kmitul1993@gmail.com>
2021-05-29 14:01:03 +0530

    amazone clone with Tailwind + Next.js

---
+++ README.md
@@ -0,0 +1,21 @@
+### Amzone clone using Next.js / Tailwind CSS / Redux / Firebase / NextAuth
+
+# Installation Steps
+
+## Using npm
+
+Run commands
+
+1. `npm install`
+
+2. `npm run dev`
+
+## Or using yarn
+
+Run commands
+
+1. `npm install --global yarn`
+
+2. `yarn install`
+
+3. `yarn run dev`
---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  // mode: "jit",
+  purge: [
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        amazon_blue: {
+          light: "#232F3E",
+          DEFAULT: "#131921",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      gradientColorStops: ['active'],
+    },
+  },
+  plugins: [require("@tailwindcss/line-clamp")],
+};
